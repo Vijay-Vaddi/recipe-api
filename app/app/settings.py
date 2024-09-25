fb137de90d78022cdb6aaf63f059a0d255b683cc
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-09-25 16:45:02 +0100

    postgres settings params

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +84,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('RECIPE_DB_NAME'),
+        'HOST': os.environ.get('RECIPE_DB_HOST'),
+        'USER': os.environ.get('RECIPE_DB_USER'),
+        'PASSWORD': os.environ.get('RECIPE_DB_PASSWORD'),
+
     }
 }
 
