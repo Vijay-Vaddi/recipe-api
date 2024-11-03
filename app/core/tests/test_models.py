e5a17de5ad8838cd73058dc77fda40df9b9b152b
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-11-03 19:00:36 +0000

    test for create user email with success

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,20 @@
+"""Tests for models """
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    """Test models"""
+
+    def test_create_user_with_email_successful(self):
+        """Tests for creating user with email if success"""
+        email = "vijay@gmail.com"
+        password = "Hello123"
+
+        user = get_user_model.objects.create_user(
+            email=email, password=password)
+
+        self.assertEqual(self.email, email)
+        self.assertTrue(self.check_password(password))
+
+        
